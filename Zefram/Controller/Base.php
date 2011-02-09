f699db7de5823830649b5f4ea4169a7a73acd836
xemlock <xemlock@gmail.com>
2011-02-09 17:26:49 +0000

    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@89 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Controller/Base.php
+++ Zefram/Controller/Base.php
@@ -4,14 +4,6 @@ require_once 'Zend/Controller/Action.php';
 
 abstract class Zefram_Controller_Base extends Zend_Controller_Action
 {
-    public function init() 
-    {
-        parent::init();
-
-        // FIXME to nie jest konieczne. baseUrl mozna wyczytac z _request
-        $this->view->baseUrl = Zend_Registry::get('baseUrl');
-    }
-
     protected function _isAjaxRequest()
     {
         return $this->_request->isXmlHttpRequest() || (null !== $this->_request->getParam('ajax'));
