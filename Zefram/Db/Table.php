5bc97836829493535a2ade4bc9b9ee240df42bae
xemlock <xemlock@gmail.com>
2011-02-15 10:45:31 +0000

    Db/Table: - extends Zend_Db_Table_Abstract with getName() and getSchema() methods for convenience
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@96 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Db/Table.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Zefram_Db_Table extends Zend_Db_Table_Abstract
+{
+    // does anybody know why these are missing in Zend_Db
+    // info() is extremely inconvenient
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    public function getSchema()
+    {
+        return $this->_schema;
+    }
+}
