8c71983c77b0ca25990f907f74671831f97be846
xemlock <xemlock@gmail.com>
2011-03-09 10:33:37 +0000

    Replacement for Zend_Controller_Action_Helper_FlashMessenger working under PHP 5.2.0
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@112 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Controller/Action/Helper/FlashMessenger.php
@@ -0,0 +1,21 @@
+<?php
+
+// According to http://stackoverflow.com/questions/1086075/zend-framework-flashmessenger-problem
+// there are PHP 5.2.x versions, that have a problem with
+// Zend_Controller_Action_Helper_FlashMessenger::addMessage() (line 143)
+// self::$_session->{$this->_namespace}[] = $message;
+// 
+class Zefram_Controller_Action_Helper_FlashMessenger extends Zend_Controller_Action_Helper_FlashMessenger
+{
+    public function addMessage($message)
+    {
+        parent::addMessage($message);
+        $messages = self::$_session->{$this->_namespace};
+        $count = count($messages);
+        if (!$count || $messages[$count - 1] != $message) {
+            $messages[] = $message;
+            self::$_session->{$this->_namespace} = $messages;
+        }
+        return $this;
+    }
+}
