fa86563d8fc969612cfaf91f0670386bb284f4a0
xemlock <xemlock@gmail.com>
2011-03-11 14:06:48 +0000

    Zefram_Controller_Action: - runUnit implementation
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@116 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Controller/Action.php
+++ Zefram/Controller/Action.php
@@ -1,7 +1,37 @@
 <?php
 
 class Zefram_Controller_Action extends Zend_Controller_Action
-[
+{
+    public function runUnit()
+    {
+        $controllerName = $this->_request->getControllerName();
+        $controller = preg_replace('/Controller$/i', '', get_class($this));
 
+        $actionName = $this->_request->getActionName();
+        $action = preg_replace_callback(
+            '/-([a-zA-Z0-9]+)/', 
+            create_function('$matches', 'return ucfirst($matches[1]);'),
+            $actionName
+        );
 
-]
+        $dir = Zend_Controller_Front::getInstance()->getModuleDirectory() . '/' .
+               Zend_Controller_Front::getInstance()->getModuleControllerDirectoryName();
+        $file = $dir . '/' . $controller . '/' . $action . '.php';
+
+        include_once $file;
+
+        $unitClass = $controller . '_' . $action;
+        if (!class_exists($unitClass, false)) {
+            throw new Exception(sprintf('Unit action file not found: %s', $file));
+        }
+
+        $args = func_get_args();
+        while (count($args) < 5) {
+            $args[] = null;
+        }
+        $unit = new $unitClass($this, $args[0], $args[1], $args[2], $args[3], $args[4]);
+
+        return $unit->run();
+    }
+
+}
