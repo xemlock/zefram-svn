04707a7741679ab53ca8d1d08263701ac1415e29
xemlock <xemlock@gmail.com>
2011-07-04 12:27:50 +0000

    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@143 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Controller/Model.php
+++ Zefram/Controller/Model.php
@@ -8,7 +8,7 @@ class Zefram_Controller_Model extends Zefram_Controller_Action
     {
         $action = new Zefram_Controller_Action_Unit_Model($this, array(
             'modelName' => $this->_modelName,
-            'mode' => Zefram_Controller_ACtion_Unit_Model::CREATE,
+            'mode' => Zefram_Controller_Action_Unit_Model::CREATE,
         ));
         return $action->run();
     }
@@ -17,7 +17,7 @@ class Zefram_Controller_Model extends Zefram_Controller_Action
     {
         $action = new Zefram_Controller_Action_Unit_Model($this, array(
             'modelName' => $this->_modelName,
-            'mode' => Zefram_Controller_ACtion_Unit_Model::UPDATE,
+            'mode' => Zefram_Controller_Action_Unit_Model::UPDATE,
         ));
         return $action->run();
     }
