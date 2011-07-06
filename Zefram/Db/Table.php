5adbee6153076e857b2e8a202f8e7424fb45dba0
xemlock <xemlock@gmail.com>
2011-07-06 13:08:02 +0000

    Zefram_Db_Table: - fetchRow: integer $where argument is treated as a condition for primary key value
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@144 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Db/Table.php
+++ Zefram/Db/Table.php
@@ -13,4 +13,21 @@ class Zefram_Db_Table extends Zend_Db_Table_Abstract
     {
         return $this->_schema;
     }
+
+    // numeric $where value is treated as pk = value condition
+    // (works just like find but returns single row on success
+    // rather than rowset).
+    public function fetchRow($where = null, $order = null)
+    {
+        if (is_scalar($where) && ctype_digit($where)) {
+            // numeric primary key
+            $primary = $this->info(Zend_Db_Table_Abstract::PRIMARY);
+            if (count($primary) == 1) {
+                // only scalar primary key
+                $primary = reset($primary);
+                $where = array($primary . ' = ?' => $where);
+            }
+        }
+        return parent::fetchRow($where, $order);
+    }
 }
