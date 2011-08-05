b94463455c25ecc7869bd0f64768a374f9fecefb
xemlock <xemlock@gmail.com>
2011-08-05 11:53:50 +0000

    still working...
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@155 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Controller/Action/Standalone/Form.php
+++ Zefram/Controller/Action/Standalone/Form.php
@@ -1,15 +1,58 @@
 <?php
 
 /**
- * Form handling logic
- * @depends Zend_Form
+ * Standalone action to handle form related logic.
+ * Main reason for existence of this class is to avoid repetitively writing
+ * form handling code.
+ *
+ * @category   Zefram
+ * @package    Zefram_Controller
+ * @subpackage Zefram_Controller_Action
+ * @copyright  Copyright (c) 2011 Xemlock
+ * @license    MIT License
  */
 class Zefram_Controller_Action_Standalone_Form extends Zefram_Controller_Action_Standalone_Abstract
 {
-    const XMLHTTP_TYPE_FORM_MARKUP = 'FormHTML';
-    const XMLHTTP_TYPE_FORM_ERRORS = 'FormErrors';
+    const RESPONSE_OK      = 'ok';
+    const RESPONSE_ERROR   = 'error';
+    const RESPONSE_DEFAULT = 'default';
 
-    protected $_form;
+    /**
+     * Report form processing errors embedded in the form's markup
+     * @var string
+     */
+    const XMLHTTP_RESPONSE_HTML   = 'FormHTML';
+
+    /**
+     * Report form processing errors as an object
+     * @var string
+     */
+    const XMLHTTP_RESPONSE_ERRORS = 'FormErrors';
+
+    /**
+     * Use partial or full form processing
+     * @var bool
+     */
+    protected $_processPartial = false;
+
+    /**
+     * Return response suitable for AJAX handling, regardless of whether 
+     * action was accessed through AJAX or not
+     * @var bool
+     */
+    protected $_xmlHttpOnly = false;
+
+    /**
+     * How form errors should be reported when using AJAX
+     * @var string
+     */
+    protected $_xmlHttpErrorResponseType = self::XMLHTTP_RESPONSE_ERRORS;
+
+    /**
+     * Form to process
+     * @var Zend_Form
+     */
+    protected $_form = null;
 
     /**
      * if second arg is instance of Zend_Form it is used as a form,
@@ -17,48 +60,79 @@ class Zefram_Controller_Action_Standalone_Form extends Zefram_Controller_Action_
      * are passed to it.
      * When overriding constructor in subclesses call it with Zend_Form
      * parameter to avoid additional form creation.
+     * controller, Zend_Form $form
+     * controller, array | Zend_Config $options
+     * controller, ... - args ... will be passed to initForm
      */
-    public function __construct(Zend_Controller_Action $controller)
+    public function __construct(Zend_Controller_Action $controller) // {{{
     {
-        parent::__construct($controller);
-
-        if (func_num_args() > 1 && ($form = func_get_arg(1)) instanceof Zend_Form) {
-            $this->_form = $form;
-        } else {
-            $args = func_get_args();
-            array_shift($args);
-            $this->_form = call_user_func_array(array($this, 'initForm'), $args);
-        }
-    }
-
-    public function getForm()
-    {
-        return $this->_form;
-    }
-    
-    protected function _process(array $values)
-    {}
-
-    public function init() 
-    {
-        parent::init();
-        $this->_bulletin = Bulletin::fetchBulletin($this->_getParam('b'));        
-    }
-
-    protected function _processPartial(array $partialValues)
-    {
-        if (count($partialValues)) {
-            $editable = array('title', 'description');
-            foreach ($editable as $column) {
-                if (isset($partialValues[$column])) {
-                    $this->_bulletin->$column = $partialValues[$column];
+        if (func_num_args() > 1) {
+            $arg = func_get_arg(1);
+            if ($arg instanceof Zend_Form) {
+                $this->_form = $arg;
+            } else {
+                if ($arg instanceof Zend_Config) {
+                    $arg = $arg->toArray();
+                }
+                if (is_array($arg)) {
+                    // read config values from array
+                    if (isset($arg['form']) && $arg['form'] instanceof Zend_Form) {
+                        $this->_form = $arg['form'];
+                    }
+                    if (isset($arg['processPartial'])) {
+                        $this->_processPartial = (bool) $arg['processPartial'];
+                    }
+                    if (isset($arg['xmlHttpOnly'])) {
+                        $this->_xmlHttpOnly = (bool) $arg['xmlHttpOnly'];
+                    }
+                    if (isset($arg['xmlHttpErrorResponseType'])) {
+                        $this->_xmlHttpErrorResponseType = (string) $arg['xmlHttpErrorResponseType'];
+                    }
                 }
             }
-            $this->_bulletin->save();
-            Bulletin::clearCache();
-            return $this->_bulletin->toArray();
         }
-    }
+        if (null === $this->_form) {
+            // call initForm with all but first parameters passed to the constructor
+            $args = func_get_args();
+            array_shift($args);
+            $form = call_user_func_array(array($this, 'initForm'), $args);
+            if (!$form instanceof Zend_Form) {
+                throw new Zefram_Exception('initForm() must return an instance of Zend_Form');
+            }
+            $this->_form = $form;
+        }
+        parent::__construct($controller);
+    } // }}}
+
+    public function initForm() // {{{
+    {
+        throw new Zefram_Exception(__METHOD__ . '() is not implemented');
+    } // }}}
+
+    public function getForm() // {{{
+    {
+        return $this->_form;
+    } // }}}
+
+    /**
+     * Process valid form
+     *
+     * @param array $values array of form values
+     */
+    protected function _process(array $values) // {{{
+    {
+        throw new Zefram_Exception(__METHOD__ . '() is not implemented');
+    } // }}}
+
+    /**
+     * Process partially valid form
+     *
+     * @param array $partialValues array of valid values
+     */
+    protected function _processPartial(array $partialValues) // {{{
+    {
+        throw new Zefram_Exception(__METHOD__ . '() is not implemented');
+    } // }}}
 
     /*
     Browser UI:
@@ -112,73 +186,80 @@ FIXME only toplevel custom errors!!!
         return $messages;
     } // }}}
 
+    /**
+     * Method-aware retrieval of submitted form data
+     */
+    protected function _getSubmitData() // {{{
+    {
+        $method = $this->_form->getMethod();
+        $request = $this->getRequest();
+
+        if (!strcasecmp($method, 'POST') && $request->isPost()) {
+            return $request->getPost();
+        }
+        if (!strcasecmp($method, 'GET') && $request->isGet()) {
+            return $request->getGet();
+        }
+        return null;
+    } // }}}
+
+    /**
+     * Execute form handling logic
+     */
     public function run()
     {
-        $form = new Zefram_Form;
-        $form->addElement('text', 'title', array('required' => true));
-        $form->addElement('text', 'description');
-        
-        // config -----------------------------------------------------
-        $partial = true;
-        $requireAjax = true;
-        $ajaxResponse = self::AJAX_FORM_MARKUP;
-
-        // form handling and rendering --------------------------------
+        $form = $this->_form;
         $request = $this->getRequest();
         $controller = $this->getController();
-        $isAjax  = $requireAjax || $request->isXmlHttpRequest();
 
-        $this->_form = $form;
-        $view = $this->getView();
-        $view->form = $form;
+        $isXmlHttp = $this->_xmlHttpOnly || $request->isXmlHttpRequest();
 
         if (($submitData = $this->_getSubmitData()) !== null) {
-            $validData = null;
-            $processed = false;
+            $isProcessed = false;
             try {
-                if ($partial) {
+                if ($this->_processPartial) {
                     $validData = $this->_form->getValidValues($submitData);
                     if (count($validData)) {
                         $result = $this->_processPartial($validData);
-                        $processed = true;
+                        $isProcessed = true;
                     }
                 } else {
-                    if ($this->_form->isValid($submitData)) {
-                        $result = $this->_process($this->_form->getValues());
-                        $processed = true;
+                    if ($form->isValid($submitData)) {
+                        $result = $this->_process($form->getValues());
+                        $isProcessed = true;
                     }
                 }
 
-                // string result is considered as a redirect uri
-                if ($isAjax) {
-                    if ($processed) {
+                if ($isXmlHttp) {
+                    // if accessed through AJAX return proper response
+                    if ($isProcessed) {
                         $response = array('status' => 'ok');
                         if (!empty($result)) {
                             $response['data'] = $result;
                         }
-                        return $this->_json($response);
                     } else {
-                        // if accessed with AJAX rend proper response
-                        $response = array('status' => 'error');
-
-                        switch ($ajaxResponse) {
-                            case self::AJAX_FORM_MARKUP:
-                                $response['type'] = 'FormHTML';
-                                $view->doctype('XHTML1_STRICT');
-                                $form->setView($view);
-                                $response['data'] = '<html>' . $form->__toString() . '</html>';
-                                break;
-
-                            case self::AJAX_FORM_ERRORS:
-                                $response['type'] = 'FormErrors';
-                                $response['data'] = array(
+                        if (self::XMLHTTP_RESPONSE_HTML === $this->_xmlHttpErrorResponseType) {
+                            $doctype = $view->getDoctype();
+                            $view->doctype('XHTML1_STRICT');
+                            $form->setView($view);
+                            $response = array(
+                                'status' => 'error',
+                                'type'   => self::XMLHTTP_RESPONSE_HTML,
+                                'data'   => '<html>' . $form->__toString() . '</html>',
+                            );
+                            $view->doctype($doctype);
+                        } else {
+                            $response = array(
+                                'status' => 'error',
+                                'type'   => self::XMLHTTP_RESPONSE_ERRORS,
+                                'data'   => array(
                                     'form'     => $form->getCustomMessages(),
                                     'elements' => $this->_getErrorMessages($form),
-                                );
-                                break;
+                                ),
+                            );
                         }
-                        return $this->_json($response);
                     }
+                    return $this->_json($response);
                 } else {
                     // redirect
 
@@ -197,6 +278,8 @@ FIXME only toplevel custom errors!!!
             }
         }
 
+        $view = $this->getView();
+        $view->form = $form;
         $template = $view->getScriptPath($controller->getViewScript());
         if ((false === $template) || !file_exists($template)) {
             // show form even if template does not exist
@@ -224,3 +307,5 @@ FIXME only toplevel custom errors!!!
         $this->getHelper('viewRenderer')->setNoRender();
     }
 }
+
+// vim: sw=4 et fdm=marker
