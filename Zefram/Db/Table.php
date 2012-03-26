324442e09a6983ba630824df2aa246929f24277e
xemlock <xemlock@gmail.com>
2012-03-26 07:36:32 +0000

    Db/Table: - fetchAllAsArray(), fetchRowAsArray(): it is good to have choice whether to fetch objects or arrays - fixed detecting scalar & integer primary key conditions and moved to _wherePrimary()
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@165 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Db/Table.php
+++ Zefram/Db/Table.php
@@ -2,6 +2,57 @@
 
 class Zefram_Db_Table extends Zend_Db_Table_Abstract
 {
+    /**
+     * Fetches all rows, but returns them as arrays instead of objects.
+     * See {@link Zend_Db_Table_Abstract::fetchAll()} for parameter explanation.
+     *
+     * @return array
+     */
+    public function fetchAllAsArray($where = null, $order = null, $count = null, $offset = null)
+    {
+        $select = $this->select();
+
+        if (null !== $where) {
+            $this->_where($select, $where);
+        }
+        
+        if ($order !== null) {
+            $this->_order($select, $order);
+        }
+
+        if ($count !== null || $offset !== null) {
+            $select->limit($count, $offset);
+        }
+
+        return $this->getAdapter()->fetchAll($select, null, Zend_Db::FETCH_ASSOC);
+    }
+
+    /**
+     * Fetches one row as array or returns false if no row matches the
+     * specified criteria. See {@link Zend_Db_Table_Abstract::fetchRow()}
+     * for parameter explanation. 
+     *
+     * @return array|false
+     */
+    public function fetchRowAsArray($where = null, $order = null, $offset = null)
+    {
+        $select = $this->select();
+
+        if ($where !== null) {
+            $this->_where($select, $this->_wherePrimary($where));
+        }
+
+        if ($order !== null) {
+            $this->_order($select, $order);
+        }
+
+        $select->limit(1, is_numeric($offset) ? intval($offset) : null);
+
+        $row = $this->getAdapter()->fetchRow($select, null, Zend_Db::FETCH_ASSOC);
+        return empty($row) ? false : $row;
+    }
+
+
     // does anybody know why these are missing in Zend_Db
     // info() is extremely inconvenient
     public function getName($quote = false)
@@ -21,15 +72,25 @@ class Zefram_Db_Table extends Zend_Db_Table_Abstract
     // rather than rowset).
     public function fetchRow($where = null, $order = null)
     {
-        if (is_scalar($where) && ctype_digit($where)) {
+        
+        return parent::fetchRow($this->_wherePrimary($where), $order);
+    }
+
+    /**
+     * If parameter is scalar and integer it is converted to
+     * primary key condition.
+     */
+    protected function _wherePrimary($where)
+    {
+        if (is_scalar($where) && ($where == intval($where))) {
             // numeric primary key
             $primary = $this->info(Zend_Db_Table_Abstract::PRIMARY);
             if (count($primary) == 1) {
                 // only scalar primary key
                 $primary = reset($primary);
-                $where = array($primary . ' = ?' => $where);
+                $where = array($primary . ' = ?' => intval($where));
             }
         }
-        return parent::fetchRow($where, $order);
+        return $where;
     }
 }
