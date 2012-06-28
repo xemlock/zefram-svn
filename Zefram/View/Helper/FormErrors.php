0bb1086bd443afc7b555af4a4090a52ec671f255
xemlock <xemlock@gmail.com>
2012-06-28 09:20:47 +0000

    View_Helper_FormErrors: - support for Zend_Form_Element as error messages source
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@192 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/View/Helper/FormErrors.php
+++ Zefram/View/Helper/FormErrors.php
@@ -5,26 +5,34 @@ class Zefram_View_Helper_FormErrors extends Zend_View_Helper_FormErrors
     /**
      * Render form errors
      *
-     * @param  string|array|Zend_Form $source Error(s) or form to render errors from
+     * @param  string|array|Zend_Form|Zend_Form_Element $source Error(s) or form to render errors from
      * @param  array $options
      * @return string
      */
     public function formErrors($source, array $options = null)
     {
-        $form = $source instanceof Zend_Form ? $source : null;
-        if ($form) {
-            if (!$form->isErrors()) {
-                return '';
-            }
-            // use FormErrors decorator if available
-            $formErrors = $form->getDecorator('FormErrors');
-            if ($formErrors) {
-                $formErrors->setElement($form);
-                return $formErrors->render('');
-            }
-            // show only custom error messages
-            $source = $form->getCustomMessages();
+        switch (true) {
+            case $source instanceof Zend_Form:
+                if (!$source->isErrors()) {
+                    return '';
+                }
+                // use FormErrors decorator if available
+                $formErrors = $source->getDecorator('FormErrors');
+                if ($formErrors) {
+                    $formErrors->setElement($source);
+                    return $formErrors->render('');
+                }
+                // show only custom error messages
+                $source = $source->getCustomMessages();
+                break;
+
+            case $source instanceof Zend_Form_Element:
+                $source = $source->getMessages();
+                break;
         }
-        return parent::formErrors($source, $options);
+
+        return empty($source)
+             ? ''
+             : parent::formErrors($source, $options);
     }
 }
