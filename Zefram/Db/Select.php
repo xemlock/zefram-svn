68088bb0f2fcd9025daef21895fde587c57bf305
xemlock <xemlock@gmail.com>
2012-10-14 11:48:57 +0000

    Zefram_Db_Select: - where() handles array condition the same way as in Zend_Db_Table_Abstract::fetchAll()
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@207 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Db/Select.php
+++ Zefram/Db/Select.php
@@ -11,4 +11,36 @@ class Zefram_Db_Select extends Zend_Db_Select
     {
         return $this->query($fetchMode)->fetchAll();
     }
+
+    /**
+     * Adds a WHERE conditions to the query by AND.
+     *
+     * @param array|string $cond
+     *     The WHERE condition. If array, treat its elements the same way as
+     *     $where parameter in {@see Zend_Db_Table_Abstract::fetchAll()}.
+     * @param mixed $value
+     *     OPTIONAL The value to quote into the condition.
+     * @param int $type
+     *     OPTIONAL The type of the given value
+     * @return Zefram_Db_Select This object.
+     */
+    public function where($cond, $value = null, $type = null)
+    {
+        if (is_array($cond)) {
+            foreach ($where as $key => $val) {
+                if (is_int($key)) {
+                    // $val is the full condition
+                    parent::where($val);
+                } else {
+                    // $key is the condition with placeholder,
+                    // and $val is quoted into the condition
+                    parent::where($key, $val);
+                }
+            }
+        } else {
+            parent::where($cond, $value, $type);
+        }
+
+        return $this;
+    }
 }
