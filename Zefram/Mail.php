96e79e493be40e9bb23976733657927c9b8bdc41
xemlock <xemlock@gmail.com>
2013-04-23 21:59:22 +0000

    Zefram_Mail: - {get,set,clear}DefaultCharset(), {get,set,clear}DefaultHeaderEncoding()
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@259 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Mail.php
@@ -0,0 +1,65 @@
+<?php
+
+class Zefram_Mail extends Zend_Mail
+{
+    /**
+     * Default Mail character set
+     * @var string
+     * @static
+     */
+    protected static $_defaultCharset = null;
+
+    /**
+     * Encoding of Mail headers
+     * @var string
+     * @static
+     */
+    protected static $_defaultHeaderEncoding = null;
+
+    public static function setDefaultCharset($charset)
+    {
+        self::$_defaultCharset = (string) $charset;
+    }
+
+    public static function getDefaultCharset()
+    {
+        return self::$_defaultCharset;
+    }
+
+    public static function clearDefaultCharset()
+    {
+        self::$_defaultCharset = null;
+    }
+
+    public static function setDefaultHeaderEncoding($headerEncoding)
+    {
+        self::$_defaultHeaderEncoding = (string) $headerEncoding;
+    }
+
+    public static function getDefaultHeaderEncoding()
+    {
+        return self::$_defaultHeaderEncoding;
+    }
+
+    public static function clearDefaultHeaderEncoding()
+    {
+        self::$_defaultHeaderEncoding = null;
+    }
+
+    public function __construct($charset = null)
+    {
+        if (null === $charset && null !== self::$_defaultCharset) {
+            $charset = self::$_defaultCharset;
+        }
+
+        parent::__construct($charset);
+
+        if (null !== self::getDefaultFrom()) {
+            $this->setFromToDefaultFrom();
+        }
+
+        if (null !== self::$_defaultHeaderEncoding) {
+            $this->setHeaderEncoding(self::$_defaultHeaderEncoding);
+        }
+    }
+}
