199d24e6a36775a2b45c951a36dc4f79f0290fec
xemlock <xemlock@gmail.com>
2013-06-21 12:06:46 +0000

    typo
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@284 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Mail/Transport/Smtp.php
+++ Zefram/Mail/Transport/Smtp.php
@@ -5,6 +5,6 @@ class Zefram_Mail_Transport_Smtp extends Zend_Mail_Transport_Smtp
     public function __construct(array $config = array())
     {
         $host = isset($config['host']) ? $config['host'] : '127.0.0.1';
-        parent::__construct($config);
+        parent::__construct($host, $config);
     }
 }
