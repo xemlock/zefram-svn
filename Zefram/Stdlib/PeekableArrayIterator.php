553ff114e265d4a5665b21d6d482e864800d7552
xemlock <xemlock@gmail.com>
2013-08-10 18:22:34 +0000

    Stdlib: - PeekableIterator interface - PeekableArrayIterator: PeekableIterator operating on an array
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@333 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Stdlib/PeekableArrayIterator.php
@@ -0,0 +1,89 @@
+<?php
+
+class Zefram_Stdlib_PeekableArrayIterator 
+    implements Iterator, Countable, ArrayAccess, Zefram_Stdlib_PeekableIterator
+{
+    /**
+     * @var array
+     */
+    protected $_array;
+
+    public function __construct(array $array = array())
+    {
+        $this->_array = $array;
+    }
+
+    public function current()
+    {
+        return current($this->_array);
+    }
+
+    public function key()
+    {
+        return key($this->_array);
+    }
+
+    public function next()
+    {
+        return next($this->_array);
+    }
+
+    public function rewind()
+    {
+        return reset($this->_array);
+    }
+
+    public function valid()
+    {
+        return key($this->_array) !== null;
+    }
+
+    public function count()
+    {
+        return count($this->_array);    
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->_array[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->_array[$offset]) ? $this->_array[$offset] : null;
+    }
+
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->_array[] = $value;
+        } else {
+            $this->_array[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->_array[$offset]);
+    }
+
+    public function peek()
+    {
+        if ($this->valid()) {
+            $value = next($this->_array);
+            prev($this->_array);
+            return $value;
+        }
+        return false;
+    }
+
+    public function hasNext()
+    {
+        if ($this->valid()) {
+            next($this->_array);
+            $result = $this->valid();
+            prev($this->_array);
+            return $result;
+        }
+        return false;
+    }
+}
---
+++ Zefram/Stdlib/PeekableIterator.php
@@ -0,0 +1,19 @@
+<?php
+
+interface Zefram_Stdlib_PeekableIterator extends Iterator
+{
+    /**
+     * Returns the next element in the iteration, without advancing
+     * the iteration.
+     *
+     * @return mixed
+     */
+    public function peek();
+
+    /**
+     * Returns true if the iteration has more elements.
+     *
+     * @return bool
+     */
+    public function hasNext();
+}
