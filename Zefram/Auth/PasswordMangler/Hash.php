a2215fe90e233c40fea9e36a213c1302dad922ea
xemlock <xemlock@gmail.com>
2013-09-13 13:28:36 +0000

    Auth_PasswordMangler_Hash: - implemented key stretching and iterated hashing (backwards compatibility retained)
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@339 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Auth/PasswordMangler/Hash.php
+++ Zefram/Auth/PasswordMangler/Hash.php
@@ -6,20 +6,44 @@
  * @category   Zefram
  * @package    Zefram_Auth
  * @copyright  Copyright (c) 2013 xemlock
- * @version    2013-07-01
+ * @version    2013-09-13
  */
 class Zefram_Auth_PasswordMangler_Hash extends Zefram_Auth_PasswordMangler
 {
+    const ROUND_COUNT = 8192;
+
     protected $_hashName;
     protected $_saltSeparator = '.';
+    protected $_rounds;
 
-    public function __construct($hashName)
+    /**
+     * @param string $hashName
+     * @param int $rounds
+     */
+    public function __construct($hashName, $rounds = self::ROUND_COUNT)
     {
         $this->_hashName = $hashName;
+
+        if (!is_int($rounds) || $rounds <= 0) {
+            throw new InvalidArgumentException('Round count must a positive integer');
+        }
+        $this->_rounds = $rounds;
     }
 
     public function validate($password, $challenge, $context = null)
     {
+        if (substr($challenge, 0, 3) === '$M$') {
+            // $M${rounds}${salt}${hash}
+            $parts = explode('$', substr($challenge, 3), 3);
+            if (count($parts) === 3) {
+                list($rounds, $salt, ) = $parts;
+                return $this->mangle($password, $salt, $rounds) == $challenge;
+            } else {
+                return false;
+            }
+        }
+
+        // legacy code
         $pos = strpos($challenge, $this->_saltSeparator);
 
         if ($pos === false) {
@@ -34,23 +58,44 @@ class Zefram_Auth_PasswordMangler_Hash extends Zefram_Auth_PasswordMangler
 
     /**
      * @param  string $password
-     * @param  string $salt
+     * @param  string|int $salt
+     * @param  int $rounds
      * @return string
      */
-    public function mangle($password, $salt = null)
+    public function mangle($password, $salt = null, $rounds = null)
     {
         if (null === $salt) {
+            // If salt is not given generate a (pseudo-)random string
+            $salt = 16;
+        }
+
+        if (is_int($salt)) {
             // Generate random string used as salt during mangling process.
-            // Salt length is given a random value between 4 and 16.
-            $salt = Zefram_Math_Rand::getString(
-                mt_rand(4, 16),
-                '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-            );
+            if ($salt < 0) {
+                throw new InvalidArgumentException('Salt length must be greater than 0');
+            }
+            $salt = Zefram_Math_Rand::getString($salt, Zefram_Math_Rand::ALNUM);
         }
+
+        $salt = strval($salt);
+
+        if (null === $rounds) {
+            $rounds = $this->_rounds;
+        }
+
+        $rounds = intval($rounds);
+
+        if ($rounds <= 0) {
+            throw new InvalidArgumentException('Round count must a positive integer');
+        }
+
+        $header = '$M$' . $rounds;
         $hash = Zend_Crypt::hash($this->_hashName, $salt . $password);
-        if (strlen($salt)) {
-            $hash = $salt . $this->_saltSeparator . $hash;
+
+        while (--$rounds > 0) {
+            $hash = Zend_Crypt::hash($this->_hashName, $hash . $password);
         }
-        return $hash;
+
+        return $header . '$' . $salt . '$' . $hash;
     }
 }
