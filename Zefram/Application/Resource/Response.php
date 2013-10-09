09668ae96eb6b5ce72c602d2b4275027664a3956
xemlock <xemlock@gmail.com>
2013-10-09 14:10:08 +0000

    Zefram_Application_Resource_Response: - added ability to set cookies
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@348 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Application/Resource/Response.php
+++ Zefram/Application/Resource/Response.php
@@ -8,6 +8,13 @@
  *   resources.response.headers.NAME.value = VALUE
  *   resources.response.headers.NAME.replace = no
  *   resources.response.httpResponseCode =
+ *   resources.response.cookies.NAME = VALUE
+ *   resources.response.cookies.NAME.value = VALUE
+ *   resources.response.cookies.NAME.expire = 0
+ *   resources.response.cookies.NAME.path = /
+ *   resources.response.cookies.NAME.domain = NULL
+ *   resources.response.cookies.NAME.secure = no
+ *   resources.response.cookies.NAME.httpOnly = no
  *
  * To create response with the default options:
  *
@@ -37,6 +44,10 @@ class Zefram_Application_Resource_Response extends Zend_Application_Resource_Res
             if (isset($options['httpResponseCode'])) {
                 $this->_response->setHttpResponseCode($options['httpResponseCode']);
             }
+
+            if (isset($options['cookies']) && $this->_response->canSendHeaders(true)) {
+                $this->_setCookies($options['cookies']);
+            }
         }
 
         return $this->_response;
@@ -57,6 +68,65 @@ class Zefram_Application_Resource_Response extends Zend_Application_Resource_Res
 
                 default:
                     throw new Zend_Controller_Response_Exception("Invalid value for header '{$name}'");
+            }
+        }
+    }
+
+    protected function _setCookies(array $cookies)
+    {
+        foreach ($cookies as $name => $cookie) {
+            switch (true) {
+                case is_scalar($cookie):
+                    setcookie($name, $cookie, 0, '/');
+                    break;
+
+                case is_array($cookie) && isset($cookie['value']):
+                    $value = $cookie['value'];
+
+                    // by default expire cookie after end of current session
+                    $expire = 0;
+
+                    // use '/' instead of current directory path because of urls
+                    // format used by ZF (i.e. controller/action/key/value)
+                    $path = '/';
+
+                    // use default values of remaining paramaters, see:
+                    // http://php.net/manual/en/function.setcookie.php 
+                    $domain = null;
+                    $secure = false;
+                    $httpOnly = false;
+
+                    foreach ($cookie as $key => $val) {
+                        switch (strtolower($key)) {
+                            case 'expire':
+                                // if 'expire' value is given it is treated as
+                                // an offset in seconds from the current time
+                                $expire = time() + $val;
+                                break;
+
+                            case 'path':
+                                $path = strval($val);
+                                break;
+
+                            case 'domain':
+                                $domain = strval($val);
+                                break;
+
+                            case 'secure':
+                                $secure = (bool) $val;
+                                break;
+
+                            case 'httponly': // PHP 5.2.0
+                                $httpOnly = (bool) $val;
+                                break;
+                        }
+                    }
+
+                    setcookie($name, $value, $expire, $path, $domain, $secure, $httpOnly);
+                    break;
+
+                default:
+                    throw new Zend_Controller_Response_Exception("Invalid value for cookie '{$name}'");
             }
         }
     }
