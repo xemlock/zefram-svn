44a11c64cf529127bcbd018a4a09fec95c7776af
xemlock <xemlock@gmail.com>
2014-01-31 07:55:29 +0000

    Zefram_Form_Element_HiddenArray: - _getErrorMessages(): error messages apply to all values, not to a single one (as is the default case when isArray flag is on)
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@396 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Form/Element/HiddenArray.php
+++ Zefram/Form/Element/HiddenArray.php
@@ -9,7 +9,6 @@ class Zefram_Form_Element_HiddenArray extends Zend_Form_Element_Hidden
     public $helper = 'formHiddenArray';
 
     /**
-     * This element represents an array
      * @var bool
      */
     protected $_isArray = true;
@@ -19,4 +18,59 @@ class Zefram_Form_Element_HiddenArray extends Zend_Form_Element_Hidden
      * @var array
      */
     protected $_decorators = array('ViewHelper');
+
+    /**
+     * @return array
+     */
+    public function getValue()
+    {
+        $valueFiltered = (array) $this->_value;
+        array_walk_recursive($valueFiltered, array($this, '_filterValue'));
+        return $valueFiltered;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnfilteredValue()
+    {
+        return (array) $this->_value;
+    }
+
+    /**
+     * @var array $value
+     */
+    public function setValue($value)
+    {
+        $this->_value = (array) $value;
+        return $this;
+    }
+
+    /**
+     * Although this element represents an array, all array values are
+     * treated as a single value. Therefore error messages apply to all
+     * array of values, not to a single value.
+     *
+     * @return array
+     */
+    protected function _getErrorMessages()
+    {
+        $translator = $this->getTranslator();
+        $messages = $this->getErrorMessages();
+        $value = null;
+
+        foreach ($messages as $key => $message) {
+            if (null !== $translator) {
+                $message = $translator->translate($message);
+            }
+            if (false !== strpos($message, '%value%')) {
+                if (null === $value) {
+                    $value = implode(', ', $this->getValue());
+                }
+                $message = str_replace('%value%', $value, $message);
+            }
+            $messages[$key] = $message;
+        }
+        return $messages;
+    }
 }
