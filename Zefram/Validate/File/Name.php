4868a292a3aae3a48b5ff2c3453458902dc2b847
xemlock <xemlock@gmail.com>
2014-03-06 13:41:59 +0000

    Validate_File_Name: filename validator
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@409 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Validate/File/Name.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * File name validation. Based on:
+ * http://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
+ *
+ * @version 2014-03-06
+ */
+class Zefram_Validate_File_Name extends Zend_Validate_Abstract
+{
+    const ILLEGAL_CHARS = 'illegalChars';
+    const TOO_LONG      = 'tooLong';
+
+    protected $_messageTemplates = array(
+        self::ILLEGAL_CHARS => 'File name contains one or more illegal characters: %illegalChars%',
+        self::TOO_LONG      => 'File name length must not exceed %maxLength% characters',
+    );
+
+    protected $_messageVariables = array(
+        'illegalChars' => '_illegalChars',
+        'maxLength'    => '_maxLength',
+    );
+
+    /**
+     * @var int
+     */
+    protected $_maxLength = 255;
+
+    /**
+     * @var string
+     */
+    protected $_illegalChars = '\\ / : * ? " < > |';
+
+    /**
+     * @var string
+     */
+    protected $_illegalCharsRegex = '#[\\\/:*?"<>|]#';
+
+    /**
+     * @param  string $value
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        $valid = true;
+
+        if (function_exists('mb_strlen')) {
+            $length = mb_strlen($value);
+        } else {
+            $length = strlen($value);
+        }
+
+        if ($length >= $this->_maxLength) {
+            $this->_error(self::TOO_LONG);
+            $valid = false;
+        }
+
+        if (preg_match($this->_illegalCharsRegex, $value)) {
+            $this->_error(self::ILLEGAL_CHARS);
+            $valid = false;
+        }
+
+        return $valid;
+    }
+}
