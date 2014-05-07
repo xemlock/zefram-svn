be9158c28182ccc429afa64d0572a0c3d7841d62
xemlock <xemlock@gmail.com>
2014-05-07 09:21:41 +0000

    Zefram_View_Helper_Decorate: - render arbitrary decorator on given element
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@425 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/View/Helper/Decorate.php
@@ -0,0 +1,55 @@
+<?php
+
+class Zefram_View_Helper_Decorate extends Zend_View_Helper_Abstract
+{
+    /**
+     * Render arbitrary decorator on a given element.
+     *
+     * @param  Zend_Form_Element|Zend_Form|Zend_Form_DisplayGroup $element
+     * @param  Zend_Form_Decorator_Interface|string $decorator
+     * @param  array|string $options OPTIONAL
+     * @param  array $content OPTIONAL
+     * @return string
+     */
+    public function decorate($element, $decorator, $options = null, $content = null)
+    {
+        if ((!$element instanceof Zend_Form_Element)
+            && (!$element instanceof Zend_Form)
+            && (!$element instanceof Zend_Form_DisplayGroup))
+        {
+            throw new InvalidArgumentException(
+                'Element is expected to be an instance of Zend_Form_Element, Zend_Form or Zend_Form_DisplayGroup'
+            );
+        }
+
+        if (!$decorator instanceof Zend_Form_Decorator_Interface) {
+            $decoratorName = (string) $decorator;
+            $decorator = $element->getDecorator($decoratorName);
+
+            if (empty($decorator)) {
+                $decoratorClass = $element->getPluginLoader(Zend_Form::DECORATOR)->load($decoratorName);
+                $decorator = new $decoratorClass;
+            }
+        }
+
+        $decorator->setElement($this);
+
+        if (is_string($options)) {
+            $content = $options;
+            $options = null;
+        }
+
+        if (is_array($options)) {
+            $origOptions = $decorator->getOptions();
+            $decorator->setOptions($options);
+        }
+
+        $output = $decorator->render($content);
+
+        if (isset($origOptions)) {
+            $decorator->setOptions($origOptions);
+        }
+
+        return $output;
+    }
+}
