ef82cb49c4dcd70fa494960de5ffd5a291e66fbc
xemlock <xemlock@gmail.com>
2014-05-11 21:31:20 +0000

    Zefram_View_Helper_Gravatar: - getUrl(): does not change the state of the helper
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@428 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/View/Helper/Gravatar.php
+++ Zefram/View/Helper/Gravatar.php
@@ -4,13 +4,51 @@
  * Gravatar view helper replacement that allow getting URL of gravatar image.
  *
  * @package Zefram_View
+ * @subpackage Helper
  */
 class Zefram_View_Helper_Gravatar extends Zend_View_Helper_Gravatar
 {
-    public function getUrl($email, array $options = null)
+    /**
+     * Get avatar url.
+     *
+     * This method does not change the state of the view helper.
+     *
+     * @param  string|array $email OPTIONAL
+     * @param  array $options OPTIONAL
+     * @return string
+     */
+    public function getUrl($email = null, array $options = null)
     {
-        $this->setEmail($email);
-        $this->setOptions($options);
-        return $this->_getAvatarUrl();
+        // replace options with email if the latter is an array
+        if (is_array($email)) {
+            $options = $email;
+            $email = null;
+        }
+
+        // set email and options, but save original values for
+        // later restoration
+        if (null !== $email) {
+            $emailOrig = $this->getEmail();
+            $this->setEmail($email);
+        }
+
+        if ($options) {
+            $optionsOrig = $this->_options;
+            $this->setOptions((array) $options);
+        }
+
+        // retrieve avatar url
+        $url = $this->_getAvatarUrl();
+
+        // restore original state
+        if (isset($emailOrig)) {
+            $this->setEmail($emailOrig);
+        }
+
+        if (isset($optionsOrig)) {
+            $this->_options = $optionsOrig;
+        }
+
+        return $url;
     }
 }
