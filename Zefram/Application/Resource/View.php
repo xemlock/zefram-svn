42240dee7ccca051a9e8b754499da74133684080
xemlock <xemlock@gmail.com>
2014-07-01 14:08:57 +0000

    Zefram_Application_Resource_View: - separation of resource and view options
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@450 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Application/Resource/View.php
+++ Zefram/Application/Resource/View.php
@@ -53,65 +53,76 @@ class Zefram_Application_Resource_View extends Zend_Application_Resource_Resourc
     {
         $options = $this->getOptions();
 
-        if (isset($options['class'])) {
-            $viewClass = $options['class'];
-        } else {
-            $viewClass = 'Zend_View';
-        }
+        $resourceOptions = array_merge(array(
+            'class'                      => 'Zend_View',
+            'charset'                    => null,
+            'contentType'                => null,
+            'doctype'                    => null,
+            'headTitle'                  => null,
+            'httpEquiv'                  => null,
+            'noRender'                   => null,
+            'scriptPathNoControllerSpec' => null,
+            'scriptPathSpec'             => null,
+            'suffix'                     => null,
+            'translator'                 => null,
+        ), $options);
 
-        $view = new $viewClass($options);
+        $viewOptions = array_diff_key($options, $resourceOptions);
+        $viewClass = $resourceOptions['class'];
+
+        $view = new $viewClass($viewOptions);
 
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
         $viewRenderer->setView($view);
 
         // Set view script path specification
-        if (isset($options['scriptPathSpec'])) {
-            $viewRenderer->setViewScriptPathSpec($options['scriptPathSpec']);
+        if (isset($resourceOptions['scriptPathSpec'])) {
+            $viewRenderer->setViewScriptPathSpec($resourceOptions['scriptPathSpec']);
         }
 
         // Set view script path specification (no controller variant)
-        if (isset($options['scriptPathNoControllerSpec'])) {
-            $viewRenderer->setViewScriptPathNoControllerSpec($options['scriptPathNoControllerSpec']);
+        if (isset($resourceOptions['scriptPathNoControllerSpec'])) {
+            $viewRenderer->setViewScriptPathNoControllerSpec($resourceOptions['scriptPathNoControllerSpec']);
         }
 
         // Set view script suffix
-        if (isset($options['suffix'])) {
-            $viewRenderer->setViewSuffix($options['suffix']);
+        if (isset($resourceOptions['suffix'])) {
+            $viewRenderer->setViewSuffix($resourceOptions['suffix']);
         }
 
         // Set the auto-render flag
-        if (isset($options['noRender'])) {
-            $viewRenderer->setNoRender($options['noRender']);
+        if (isset($resourceOptions['noRender'])) {
+            $viewRenderer->setNoRender($resourceOptions['noRender']);
         }
 
         // Set doctype using doctype view helper
-        if (isset($options['doctype'])) {
-            $view->doctype()->setDoctype(strtoupper($options['doctype']));
+        if (isset($resourceOptions['doctype'])) {
+            $view->doctype()->setDoctype(strtoupper($resourceOptions['doctype']));
         }
 
         // Create an HTML5-style meta charset tag using headMeta view helper
-        if (isset($options['charset'])) {
+        if (isset($resourceOptions['charset'])) {
             if (!$view->doctype()->isHtml5()) {
                 throw new Zend_View_Exception('Meta charset tag requires an HTML5 doctype');
             }
-            $view->headMeta()->setCharset($options['charset']);
+            $view->headMeta()->setCharset($resourceOptions['charset']);
         }
 
         // Set content-type meta tag using headMeta view helper
-        if (isset($options['contentType'])) {
-            $view->headMeta()->appendHttpEquiv('Content-Type', $options['contentType']);
+        if (isset($resourceOptions['contentType'])) {
+            $view->headMeta()->appendHttpEquiv('Content-Type', $resourceOptions['contentType']);
         }
 
         // Add http-equiv meta tags using headMeta view helper
-        if (isset($options['httpEquiv'])) {
-            foreach ($options['httpEquiv'] as $key => $value) {
+        if (isset($resourceOptions['httpEquiv'])) {
+            foreach ($resourceOptions['httpEquiv'] as $key => $value) {
                 $view->headMeta()->appendHttpEquiv($key, $value);
             }
         }
 
         // Set head title
-        if (isset($options['headTitle'])) {
-            foreach ($options['headTitle'] as $key => $value) {
+        if (isset($resourceOptions['headTitle'])) {
+            foreach ($resourceOptions['headTitle'] as $key => $value) {
                 switch ($key) {
                     case 'title':
                         $view->headTitle()->set($value);
@@ -131,8 +142,8 @@ class Zefram_Application_Resource_View extends Zend_Application_Resource_Resourc
         // Set a translation adapter for translate view helper
         // (To avoid duplicated resources or cyclic dependency exceptions
         // the bootstrapping of other resources hast to be done in init())
-        if (isset($options['translator'])) {
-            $translate = $options['translator'];
+        if (isset($resourceOptions['translator'])) {
+            $translate = $resourceOptions['translator'];
 
             if (is_string($translate)) {
                 $translate = $this->_getBootstrapResource($translate);
