9b53d9662f243f38d8ef65c9eb944fd904e511bc
xemlock <xemlock@gmail.com>
2014-07-31 03:56:39 +0000

    added Zefram_Filter_Slug
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@481 9bbe5c55-987c-480a-a901-d35e16fae7c7

---
+++ Zefram/Filter/Slug.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Filter for creating SEO friendly slugs.
+ *
+ * @version 2014-07-31
+ * @author  xemlock
+ */ 
+class Zefram_Filter_Slug extends Zefram_Filter_Translit
+{
+    public function filter($value)
+    {
+        $value = parent::filter($value);
+
+        $value = trim($value, " -_\r\n\v\f");
+        $value = preg_replace(array(
+            '/[^-0-9a-z]/i',
+            '/-+/',
+        ), '-', $value);
+
+        return $value;
+    }
+}
