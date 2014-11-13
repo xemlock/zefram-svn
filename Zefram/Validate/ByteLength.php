79547d758c78db8aca027bd723c76db2a067b00f
xemlock <xemlock@gmail.com>
2014-11-13 10:08:40 +0000

    ByteLength validator update
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@507 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Validate/ByteLength.php
+++ Zefram/Validate/ByteLength.php
@@ -1,17 +1,82 @@
 <?php
 
+/**
+ * @version 2014-11-13
+ */
 class Zefram_Validate_ByteLength extends Zend_Validate_StringLength
 {
     const INVALID   = 'byteLengthInvalid';
     const TOO_SHORT = 'byteLengthTooShort';
     const TOO_LONG  = 'byteLengthTooLong';
 
+    /**
+     * @var array
+     */
     protected $_messageTemplates = array(
         self::INVALID   => "Invalid type given. String expected",
         self::TOO_SHORT => "Value is less than %min% bytes long",
         self::TOO_LONG  => "Value is more than %max% bytes long",
     );
 
+    /**
+     * @var array
+     */
+    protected $_messageVariables = array(
+        'min' => '_min',
+        'max' => '_max'
+    );
+
+    /**
+     * Minimum length
+     * @var int
+     */
+    protected $_min = 0;
+
+    /**
+     * Maximum length, no maximum length if null
+     * @var int|null
+     */
+    protected $_max;
+
+    /**
+     * Sets validator options.
+     *
+     * @param  int|array $options
+     * @return void
+     */
+    public function __construct($options = array())
+    {
+        if (is_object($options) && method_exists($options, 'toArray')) {
+            $options = (array) $options->toArray();
+        }
+
+        if (!is_array($options)) {
+            $options = func_get_args();
+
+            $temp['min'] = array_shift($options);
+            if (!empty($options)) {
+                $temp['max'] = array_shift($options);
+            }
+
+            $options = $temp;
+        }
+
+        if (array_key_exists('min', $options)) {
+            $this->setMin($options['min']);
+        }
+
+        if (array_key_exists('max', $options)) {
+            $this->setMax($options['max']);
+        }
+    }
+
+    /**
+     * Returns true if the byte length of value is at least the min option and
+     * no greater than the max option (when the max option is not null).
+     *
+     * @param  string $value
+     * @return bool
+     */
     public function isValid($value)
     {
         if (!is_string($value)) {
