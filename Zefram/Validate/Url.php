43937eb4ee640ef2b3d28ca99637acad733154d7
xemlock <xemlock@gmail.com>
2014-11-20 15:21:19 +0000

    Zefram_Validate_Url: - added allowLocal option
    git-svn-id: https://subversion.assembla.com/svn/zefram/trunk@508 9bbe5c55-987c-480a-a901-d35e16fae7c7

--- Zefram/Validate/Url.php
+++ Zefram/Validate/Url.php
@@ -6,11 +6,14 @@
  * @uses      Zefram_Url
  * @author    xemlock
  * @version   2013-07-21
+ *
+ * Changelog: 2014-11-20 Added allowLocal option
  */
 class Zefram_Validate_Url extends Zend_Validate_Abstract
 {
-    const INVALID_URL    = 'invalidUrl';
-    const INVALID_SCHEME = 'invalidScheme';
+    const INVALID            = 'urlInvalid';
+    const SCHEME_NOT_ALLOWED = 'urlSchemeNotAllowed';
+    const LOCAL_HOSTNAME     = 'urlLocalHostname';
 
     /**
      * List of allowed schemes.
@@ -19,9 +22,17 @@ class Zefram_Validate_Url extends Zend_Validate_Abstract
      */
     protected $_allowedSchemes = array('http', 'https');
 
+    /**
+     * Should local hostnames be allowed.
+     *
+     * @var bool
+     */
+    protected $_allowLocal = true;
+
     protected $_messageTemplates = array(
-        self::INVALID_URL    => "This is not a valid URL",
-        self::INVALID_SCHEME => "URL scheme '%scheme%' is not allowed",
+        self::INVALID            => "This is not a valid URL",
+        self::SCHEME_NOT_ALLOWED => "URL scheme '%scheme%' is not allowed",
+        self::LOCAL_HOSTNAME     => "Local hostname is not allowed",
     );
 
     protected $_messageVariables = array(
@@ -38,15 +49,16 @@ class Zefram_Validate_Url extends Zend_Validate_Abstract
      */
     public function __construct($options = null)
     {
-        if (null !== $options) {
-            if (is_object($options) && method_exists($options, 'toArray')) {
-                $options = $options->toArray();
-            }
+        if (is_object($options) && method_exists($options, 'toArray')) {
+            $options = $options->toArray();
+        }
 
-            $options = (array) $options;
+        $options = (array) $options;
 
-            if (isset($options['allowedSchemes'])) {
-                $this->setAllowedSchemes($options['allowedSchemes']);
+        foreach ($options as $key => $value) {
+            $method = 'set' . $key;
+            if (method_exists($this, $method)) {
+                $this->{$method}($value);
             }
         }
     }
@@ -73,6 +85,24 @@ class Zefram_Validate_Url extends Zend_Validate_Abstract
     }
 
     /**
+     * @param  bool $flag
+     * @return Zefram_Validate_Url
+     */
+    public function setAllowLocal($flag)
+    {
+        $this->_allowLocal = (bool) $flag;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAllowLocal()
+    {
+        return $this->_allowLocal;
+    }
+
+    /**
      * @param  mixed $value
      * @return bool
      */
@@ -84,12 +114,12 @@ class Zefram_Validate_Url extends Zend_Validate_Abstract
         try {
             $uri = Zefram_Url::fromString($value);
         } catch (Exception $e) {
-            $this->_error(self::INVALID_URL);
+            $this->_error(self::INVALID);
             return false;
         }
 
         if (!$uri->valid()) {
-            $this->_error(self::INVALID_URL);
+            $this->_error(self::INVALID);
             return false;
         }
 
@@ -98,7 +128,20 @@ class Zefram_Validate_Url extends Zend_Validate_Abstract
         if ($this->_allowedSchemes
             && !in_array($this->_scheme, $this->_allowedSchemes, true)
         ) {
-            $this->_error(self::INVALID_SCHEME);
+            $this->_error(self::SCHEME_NOT_ALLOWED);
+            return false;
+        }
+
+        // Local domain names may end with a single dot to distinguish them
+        // from complete domain names whenever a name conflict arises, see:
+        // RFC3986 3.2.2
+
+        $host = $uri->getHost();
+
+        if ((substr($host, -1) === '.' || strpos($host, '.') === false)
+            && !$this->_allowLocal
+        ) {
+            $this->_error(self::LOCAL_HOSTNAME);
             return false;
         }
 
